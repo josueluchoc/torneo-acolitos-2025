3414f1f71866613f54be371447f37de26e74962d
Josue Lucho Cruzado <80541709+josueluchoc@users.noreply.github.com>
2025-12-03 09:57:16 -0500

    Change team status from 'Por confirmar' to 'Confirmado'

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -90,7 +90,7 @@ export const GROUPS: GroupData = {
         {
             id: 8,
             name: "Acólitos P. Espíritu Santo",
-            status: "Por confirmar",
+            status: "Confirmado",
             color: "bg-cyan-500",
             logo: logoEspirituSanto
         }
